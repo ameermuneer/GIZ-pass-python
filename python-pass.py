abd8ae73ae531472cee5f768145a515da7d7f455
Ameer Muneer <96ameermuneer@gmail.com>
2021-10-01 00:03:26 +0300

    longest palindrome class

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,19 @@
 
 
 class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def possible_palindromic(l,r):
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+        pal = ""
+        for i in range(len(s)):
+            p = possible_palindromic(i,i)
+            if len(p) > len(pal) : pal = p
+            #for even
+            p = possible_palindromic(i,i+1)
+            if len(p) > len(pal) : pal = p
+
+        return pal
